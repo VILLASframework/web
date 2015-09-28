abb65735c52cf5114366f9d1b35b2eac1f0745c6
Markus Grigull <web@grigull.me>
2015-09-28 13:45:09 +0200

    Add dynamic data generate by mirage

--- app/routes/lab-mashup.js
+++ app/routes/lab-mashup.js
@@ -2,21 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model() {
-		//return this.store.findAll('property');
-		let properties = [
-			{
-				name: "voltage",
-				value: 2.3,
-				type: "kV"
-			},
-			{
-				name: "current",
-				value: 1.6,
-				type: "A"
-			}
-		];
-		
-		return properties;
+		return this.store.findAll('property');
 	}
 });
 
