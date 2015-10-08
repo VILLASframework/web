3fd510e92649db40bad437f47056af1446c5b20b
Markus Grigull <web@grigull.me>
2015-10-08 13:35:18 +0200

    Changed layout to nav-tabs based
    Layout is work in progress

--- app/router.js
+++ app/router.js
@@ -6,10 +6,15 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-    this.route('entities', { path: '/' }, function() {
+    this.route('lab-mashup', { path: '/' }, function() {
+      // dynamic routes
       this.route('entity', { path: '/:entity_id' }, function() {
         this.route('property', { path: '/:property_id'});
       });
+
+      // static routes
+      this.route('static1', { path: '/static1' });
+      this.route('static2', { path: '/static2' });
     });
 });
 
