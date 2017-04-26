14e75c0fa0c78fa5306062aced69b4e5a3eca1ea
Ricardo Hernandez-Montoya <rhernandez@gridhound.de>
2017-04-26 12:11:21 +0200

    Very basic single passing test

--- src/Header.test.js
+++ src/Header.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Header from './components/header';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Header />, div);
 });
