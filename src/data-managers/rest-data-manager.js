342e137560db8943710d3ec80acc64241f9ab4ef
Ricardo Hernandez-Montoya <rhernandez@gridhound.de>
2017-06-01 08:35:04 +0200

    pass HTTP_PROXY environment to app

--- src/data-managers/rest-data-manager.js
+++ src/data-managers/rest-data-manager.js
@@ -22,8 +22,8 @@
 import RestAPI from '../api/rest-api';
 import AppDispatcher from '../app-dispatcher';
 
-const PROXY = process.env.REACT_APP_HTTP_PROXY || "http://localhost:4000";
-const API_URL = PROXY + '/api/v1';
+const HOST = process.env.REACT_APP_HTTP_PROXY || "";
+const API_URL = HOST + '/api/v1';
 
 class RestDataManager {
   constructor(type, url, keyFilter) {
