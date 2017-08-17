cc2766e37318fa79da60a81c3db69ff401dd4671
Markus Grigull <web@grigull.me>
2017-08-17 17:35:15 +0200

    Add home when on '/' route

--- src/containers/app.js
+++ src/containers/app.js
@@ -108,6 +108,7 @@ class App extends React.Component {
           <SidebarMenu currentRole={ this.state.currentRole }/>
 
           <div className="app-content">
+            <Route exact path="/" component={Home} />
             <Route path="/home" component={Home} />
             <Route exact path="/projects" component={Projects} />
             <Route path="/projects/:project" component={Project} />
