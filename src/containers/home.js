4f098d00c72731ecb5fe2871318331e5ebfa9ede
Steffen Vogel <post@steffenvogel.de>
2017-08-23 18:32:08 +0200

    first draft of welcome message
    missing:
     - test of stores
     - images

--- src/containers/home.js
+++ src/containers/home.js
@@ -24,6 +24,7 @@ import { Container } from 'flux/utils';
 
 // import AppDispatcher from '../app-dispatcher';
 import VillasStore from '../stores/villas-store';
+import UserStore from '../stores/users-store';
 
 class Home extends Component {
   static getStores() {
@@ -32,13 +33,41 @@ class Home extends Component {
 
   static calculateState() {
     return {
-      villas: VillasStore.getState()
+      villas: VillasStore.getState(),
+      currentUser: UserStore.getState().currentUser
+      counts: {
+        users: UserStore.getState().users.length
     };
   }
 
   render() {
     return (
       <h1>Home</h1>
+      <p>Welcome {this.state.currentUser}! This is the VILLASweb frontend version [version] hosted by [admin].</p>
+      <p>This instance is hosting {this.state.counts.projects} projects consisting of {this.state.counts.nodes} nodes, {this.state.counts.simulations} simulations and {this.state.counts.visualizations} visualisations.<p>
+      <p>There are currently {this.state.counts.users} users registered.</p>
+      <h3>Credits</h3>
+      <p>VILLASweb is developed by the Institute for Automation of Complex Power Systems at the RWTH Aachen University.</p>
+      <ul>
+        <li><a href="mailto:mgrigull@eonerc.rwth-aachen.de">Markus Grigull</a></li>
+        <li><a href="mailto:stvogel@eonerc.rwth-aachen.de">Steffen Vogel</a></li>
+        <li><a href="mailto:mstevic@eonerc.rwth-aachen.de">Marija Stevic</a></li>
+      </ul>
+      <img src="" alt="Logo ACS" />
+      <h3>Links</h3>
+      <ul>
+        <li><a href="http://fein-aachen.org/projects/villas-framework/">Project Page</a></li>
+        <li><a href="https://villas.fein-aachen.org/doc/web.html">Dokumentation</a></li>
+        <li><a href="https://git.rwth-aachen.de/VILLASframework/VILLASweb">Source Code</a></li>
+      </ul>
+      <h3>Funding</h3>
+      <p>The development of <a href="http://fein-aachen.org/projects/villas-framework/">VILLASframework</a> projects have received funding from</p>
+      <ul>
+        <li><a href="http://www.re-serve.eu">RESERVE</a> a European Union’s Horizon 2020 research and innovation programme under grant agreement No 727481</li>
+        <li><a href="http://www.jara.org/en/research/energy">JARA-ENERGY</a>. Jülich-Aachen Research Alliance (JARA) is an initiative of RWTH Aachen University and Forschungszentrum Jülich.</p>
+      </ul>
+      <img src="" alt="Logo EU" />
+      <img src="" alt="Logo JARA" />
     );
   }
 }
