6cac1df08bc6b5569b69a9b5cbc093ed5a3a8c92
Sonja Happ <sonja.happ@eonerc.rwth-aachen.de>
2020-03-13 17:20:23 +0100

    add a TODO in separate case for downloaded files (WIP)

--- src/file/file-store.js
+++ src/file/file-store.js
@@ -40,6 +40,14 @@ class FileStore extends ArrayStore {
       case 'files/upload-error':
         console.log(action.error);
         return state;
+      case 'files/loaded':
+        if (Array.isArray(action.data)) {
+          return super.reduce(state, action)
+        } else {
+          // in this case a file is contained in the response (no JSON)
+          // TODO we have to extract and process the file here (=save it somewhere?)
+          return super.reduce(state, action)
+        }
 
       default:
         return super.reduce(state, action);
