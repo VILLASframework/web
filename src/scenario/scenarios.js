ba4b3c556ee10e2be792570674f4533cc3482cb5
irismarie <Ikoester@eonerc.rwth-aachen.de>
2021-01-28 19:30:19 +0100

    changed add and impport buttons for tables

--- src/scenario/scenarios.js
+++ src/scenario/scenarios.js
@@ -241,7 +241,10 @@ class Scenarios extends Component {
 
     return (
       <div className='section'>
-        <h1>Scenarios</h1>
+        <h1>Scenarios
+          <Button onClick={() => this.setState({ newModal: true })} style={buttonStyle}><Icon icon="plus" /></Button>
+          <Button onClick={() => this.setState({ importModal: true })} style={buttonStyle}><Icon icon="upload" /></Button>
+        </h1>
 
         <Table data={this.state.scenarios}>
           <TableColumn title='Name' dataKey='name' link='/scenarios/' linkKey='id' />
@@ -259,13 +262,6 @@ class Scenarios extends Component {
             onDuplicate={index => this.duplicateScenario(index)}
           />
         </Table>
-
-        <div style={{ float: 'right' }}>
-          <Button onClick={() => this.setState({ newModal: true })} style={buttonStyle}><Icon icon="plus" /> Scenario</Button>
-          <Button onClick={() => this.setState({ importModal: true })} style={buttonStyle}><Icon icon="upload" /> Import</Button>
-        </div>
-
-        <div style={{ clear: 'both' }} />
 
         <NewScenarioDialog show={this.state.newModal} onClose={(data) => this.closeNewModal(data)} />
         <EditScenarioDialog show={this.state.editModal} onClose={(data) => this.closeEditModal(data)} scenario={this.state.modalScenario} />
