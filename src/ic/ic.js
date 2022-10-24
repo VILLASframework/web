bcab4c7b261d1cb308a0272ef2179fff86e0f6ec
Steffen Vogel <post@steffenvogel.de>
2022-10-24 15:19:32 +0200

    refactor IC status page

--- src/ic/ic.js
+++ src/ic/ic.js
@@ -124,23 +124,57 @@ class InfrastructureComponent extends React.Component {
   }
 
   static ICParamsTable(ic) {
+    let state = ic.state;
+
     return (
       <Table striped size="sm">
         <thead>
           <tr><th>Property</th><th>Value</th></tr>
         </thead>
         <tbody>
-          <tr><td>Name</td><td>{ic.name}</td></tr>
-          <tr><td>Description</td><td>{ic.description}</td></tr>
-          <tr><td>UUID</td><td>{ic.uuid}</td></tr>
-          <tr><td>State</td><td>{ic.state}</td></tr>
-          <tr><td>Category</td><td>{ic.category}</td></tr>
-          <tr><td>Type</td><td>{ic.type}</td></tr>
-          <tr><td>Uptime</td><td>{moment.duration(ic.uptime, "seconds").humanize()}</td></tr>
-          <tr><td>Location</td><td>{ic.location}</td></tr>
-          <tr><td>Websocket URL</td><td>{ic.websocketurl}</td></tr>
-          <tr><td>API URL</td><td>{ic.apiurl}</td></tr>
-          <tr><td>Start parameter schema</td><td>
+          <tr>
+            <td>Name</td>
+            <td>{ic.name}</td>
+          </tr>
+          <tr>
+            <td>Description</td>
+            <td>{ic.description}</td>
+          </tr>
+          <tr>
+            <td>UUID</td
+            ><td>{ic.uuid}</td>
+          </tr>
+          <tr>
+            <td>State</td>
+            <td>{state}</td>
+          </tr>
+          <tr>
+            <td>Category</td>
+            <td>{ic.category}</td>
+          </tr>
+          <tr>
+            <td>Type</td>
+            <td>{ic.type}</td>
+          </tr>
+          <tr>
+            <td>Uptime</td>
+            <td>{moment.duration(ic.uptime, "seconds").humanize()}</td>
+          </tr>
+          <tr>
+            <td>Location</td>
+            <td>{ic.location}</td>
+          </tr>
+          <tr>
+            <td>Websocket URL</td>
+            <td>{ic.websocketurl}</td>
+          </tr>
+          <tr>
+            <td>API URL</td>
+            <td>{ic.apiurl}</td>
+          </tr>
+          <tr>
+            <td>Start parameter schema</td>
+            <td>
               {InfrastructureComponent.isJSON(ic.startparameterschema) ?
                 <ReactJson
                   src={ic.startparameterschema}
