5954738882fad4c82a9b97cf6c4cb1d5498cc4f4
Andrii Podriez <andrey5577990@gmail.com>
2024-04-24 16:05:56 +0200

    Created userSlice to manage user authentication. Changed components related to login into function components. Organized all changed files that are related to authentication in src\pages\login\ .
    Signed-off-by: Andrii Podriez <andrey5577990@gmail.com>
    Signed-off-by: Author Name <amirhossein.nakhaei@rwth-aachen.de>

---
+++ src/pages/login/login-form.js
@@ -0,0 +1,110 @@
+/**
+ * This file is part of VILLASweb.
+ *
+ * VILLASweb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VILLASweb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VILLASweb. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+import React, { useState, useEffect } from 'react';
+import { Form, Button, Col } from 'react-bootstrap';
+import RecoverPassword from '../../user/recover-password'
+import { useDispatch } from 'react-redux'
+import { login } from '../../store/userSlice';
+import _ from 'lodash';
+
+import "./login.css";
+
+const LoginForm = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [forgottenPassword, setForgottenPassword] = useState(false)
+
+  //this variable is used to disable login button if either username or password is empty
+  const isInputValid = username !== '' && password !== '';
+
+  const dispatch = useDispatch();
+
+  const loginEvent = (event) => {
+    event.preventDefault();
+    dispatch(login({username, password}))
+  }
+
+  const villasLogin = (
+    <Form key="login_a">
+      <Form.Group controlId="username">
+        <Form.Label column={true}>Username</Form.Label>
+        <Col>
+          <Form.Control 
+            type="text" 
+            placeholder="Username" 
+            autoComplete="username" 
+            onChange={(e) => {
+              setUsername(e.target.value)
+            }
+          } />
+        </Col>
+      </Form.Group>
+
+      <Form.Group controlId="password">
+        <Form.Label column={true}>Password</Form.Label>
+        <Col >
+          <Form.Control 
+            type="password" 
+            placeholder="Password" 
+            autoComplete="current-password" 
+            onChange={(e) => {
+              setPassword(e.target.value)
+              }} 
+            />
+        </Col>
+      </Form.Group>
+
+      {props.loginMessage &&
+        <div style={{ marginBottom: '20px', color: 'red', fontSize: 'small' }}>
+          <Col sm={{ span: 10, offset: 2 }} style={{ paddingLeft: '5px' }}>
+            <i>Error: </i>{props.loginMessage}
+          </Col>
+        </div>
+      }
+
+      <Form.Group style={{ paddingTop: 15, paddingBottom: 5 }}>
+        <Col>
+        <span className='solid-button'>
+          <Button variant='secondary' style={{width: 90}} type="submit" disabled={!isInputValid} onClick={e => loginEvent(e)}>Login</Button>
+        </span>
+          <Button variant="link" size="sm" style={{marginLeft: 85, textDecoration: "none"}} onClick={() => setForgottenPassword(true)}>Forgot your password?</Button>
+        </Col>
+      </Form.Group>
+
+      <RecoverPassword show={forgottenPassword} onClose={() => setForgottenPassword(false)} sessionToken={props.sessionToken} />
+    </Form>
+  );
+
+  if (props.config) {
+    let externalLogin = _.get(props.config, ['authentication', 'external', 'enabled'])
+    let provider = _.get(props.config, ['authentication', 'external', 'provider_name'])
+    let url = _.get(props.config, ['authentication', 'external', 'authorize_url']) + "?rd=/login/complete"
+
+    if (externalLogin && provider && url) {
+      return [
+        villasLogin,
+        <hr key="login_b"/>,
+        <Button key="login_c" onClick={e => window.location = url } block>Sign in with {provider}</Button>
+      ];
+    }
+  }
+
+  return villasLogin;
+}
+
+export default LoginForm;
---
+++ src/store/userSlice.js
@@ -0,0 +1,100 @@
+/**
+ * This file is part of VILLASweb.
+ *
+ * VILLASweb is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * VILLASweb is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VILLASweb. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+
+import RestAPI from '../common/api/rest-api';
+import ICDataDataManager from '../ic/ic-data-data-manager';
+
+const userSlice = createSlice({
+    name: 'login',
+    initialState: {currentUser: null, currentToken: null, isLoading: false, loginMessage: ''},
+    extraReducers: (builder) => {
+        builder
+        .addCase(login.pending, (state, action) => {
+            state.isLoading = true
+        })
+        .addCase(login.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.currentUser = action.payload.user
+            state.currentToken = action.payload.token
+
+            localStorage.setItem('currentUser', JSON.stringify(action.payload.user));
+            localStorage.setItem('token', action.payload.token);
+        })
+        .addCase(login.rejected, (state, action) => {
+            state.loginMessage = 'Wrong credentials! Please try again.'
+        })
+        .addCase(logout.pending, (state) => {
+            state.currentUser = null
+            state.currentToken = null
+        })
+        .addCase(loginExternal.pending, (state, action) => {
+            state.isLoading = true
+        })
+        .addCase(loginExternal.fulfilled, (state, action) => {
+            state.isLoading = false
+            state.currentUser = action.payload.user
+            state.currentToken = action.payload.token
+
+            localStorage.setItem('currentUser', JSON.stringify(action.payload.user));
+            localStorage.setItem('token', action.payload.token);
+        })
+    }
+})
+
+export const login = createAsyncThunk(
+    'user/login',
+    async (userData, thunkAPI) => {
+        try {
+            const res = await RestAPI.post('/api/v2/authenticate/internal', userData)
+            
+            return {user: res.user, token: res.token}
+        } catch(error) {
+            console.log('Error while trying to log in: ', error)
+            return thunkAPI.rejectWithValue(error)
+        }
+    }
+)
+
+export const loginExternal = createAsyncThunk(
+    'user/loginExternal',
+    async (data, thunkAPI) => {
+        try {
+            const res = await RestAPI.post(this.makeURL('/authenticate/external'), null, null, 60000) 
+            
+            return {user: res.user, token: res.token}
+        } catch(error) {
+            console.log('Error while trying to log in externally: ', error)
+            return thunkAPI.rejectWithValue(error)
+        }
+    }
+)
+
+export const logout = createAsyncThunk(
+    'user/logout',
+    async () => {
+        // disconnect from all infrastructure components
+        ICDataDataManager.closeAll();
+        //remove token and current user from local storage
+        localStorage.clear();
+
+        console.log("logged out")
+    }
+)
+
+export default userSlice.reducer
